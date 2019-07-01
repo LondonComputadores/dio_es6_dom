1b8eec6960c3b8356a2f4a8ff17061e387c40cc0
LondonComputadores <london.computadores@gmail.com>
2019-07-01 16:22:30 -0300

    full js recap from basic to advanced - Basic Part

---
+++ main.js
@@ -0,0 +1,18 @@
+
+var name = "Ahlex Paes";
+var age = 26;
+var age2 = 10;
+var phrase = "Vasco is better than menguinho.";
+var n1 = 5;
+var n2 = 3;
+
+alert(name + " is" + " " + age + " years old.");
+alert(age + age2);
+
+console.log(name);
+console.log(age + age2);
+console.log(phrase.replace("menguinho", "nensinho"));
+console.log(phrase.toUpperCase());
+console.log(n1 * (n2 + 7));
+
+alert(phrase.replace("menguinho", "nensinho"));
