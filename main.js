dde275c6037efbb3753ad0154cdd999d749da6a5
LondonComputadores <london.computadores@gmail.com>
2019-07-02 11:30:43 -0300

    A little more changes on this basic recap practice classes!

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
+/*
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[0].name);
+*/
+/*
+var fruta = {nome:"banana", cor:"amarela"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
 
+var lista = ["maçã", "pêra", "laranja"];
+console.log(lista);
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista.toString());
+console.log(lista.join("-"));
+
+
+
+/*
 var name = "Ahlex Paes";
 var age = 26;
 var age2 = 10;
@@ -16,3 +37,4 @@ console.log(phrase.toUpperCase());
 console.log(n1 * (n2 + 7));
 
 alert(phrase.replace("menguinho", "nensinho"));
+*/
